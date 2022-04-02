5f616b3ce89915ca6c568522aa24b386349996a2
Kamil Bugała <kamilbugala9@gmail.com>
2022-04-02 20:35:36 +0200

    Sending more than 1 message not working

--- client.c
+++ client.c
@@ -14,34 +14,60 @@
 #include "common.h"
 
 #define BUFFER_SIZE 1024
+#define NUMBER_SIZE 30
 
 static const char exit_string[] = "exit";
 
+void send_file(FILE *fp, int sockfd){
+	char data[BUFFER_SIZE];
+
+	while(fgets(data, BUFFER_SIZE, fp) != NULL) {
+		printf("Sent: %s", data);
+		send_message(sockfd, data, BUFFER_SIZE, 0);
+	}
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fatal("Usage: %s host port \n", argv[0]);
+    if (argc != 4) {
+        fatal("Usage: %s <host> <port> <filename>\n", argv[0]);
     }
 
-    char *host = argv[1];
-    uint16_t port = read_port(argv[2]);
-    struct sockaddr_in server_address = get_address(host, port);
+	char *host = argv[1];
+	uint16_t port = read_port(argv[2]);
+	struct sockaddr_in server_address = get_address(host, port);
 
-    int socket_fd = open_socket();
+	int socket_fd = open_socket();
 
-    connect_socket(socket_fd, &server_address);
+	connect_socket(socket_fd, &server_address);
 
-    char *server_ip = get_ip(&server_address);
-    uint16_t server_port = get_port(&server_address);
+	char *server_ip = get_ip(&server_address);
+	uint16_t server_port = get_port(&server_address);
 
     printf("Connected to %s:%d\n", server_ip, server_port);
 
-    char line[BUFFER_SIZE];
-    memset(line, 0, BUFFER_SIZE);
+	FILE *f = fopen(argv[3], "r");
+	if (f == NULL) {
+		fatal("Opening file %s failed", argv[3]);
+	}
+	fseek(f, 0, SEEK_END); // seek to end of file
+	size_t file_size = ftell(f); // get current file pointer
+	fseek(f, 0, SEEK_SET); // seek back to beginning of file
 
-    do {
-        fgets(line, BUFFER_SIZE - 1, stdin);
-        send_message(socket_fd, line, strnlen(line, BUFFER_SIZE), NO_FLAGS);
-    } while (strncmp(line, exit_string, sizeof(exit_string) - 1) != 0);
+	int flags = 0;
+	char number_str[NUMBER_SIZE];
+	sprintf(number_str, "%lu", file_size);
+	strcat(argv[3], " ");
+	strcat(argv[3], number_str);
+
+	size_t message_length = strnlen(argv[3], BUFFER_SIZE);
+	printf("Message: %s\n", argv[3]);
+
+	send_message(socket_fd, argv[3], message_length, flags);
+	printf("Sent file name and file size to server %s:%d\n", server_ip, server_port);
+
+	send_file(f, socket_fd);
+	fclose(f);
+	printf("Sent file to server %s:%d successfully.\n", server_ip, server_port);
 
     CHECK_ERRNO(close(socket_fd));
 
